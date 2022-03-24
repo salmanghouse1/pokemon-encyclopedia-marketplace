263adc05806462abd2b54de1fc63b86a7c4798e3
Salman Ghouse <ghousemu@sheridancollege.ca>
2022-03-24 16:20:23 -0400

    added amazon button cdn link of yui3 to index.html

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,17 @@ import { Header, Footer } from "./components/index";
 // import { Hero } from "./components/Hero";
 // import { Footer } from "./components/Footer";
 // import { Login } from "./components/login";
+import { MantineProvider } from "@mantine/core";
 
 function App() {
   return (
+    <MantineProvider>
     <div>
       <Header />
       <Footer />
     </div>
-  );
+    </MantineProvider>
+  )
 }
 
 export default App;
