cbc83262b90acea59baaad5ff9d21f9e6f96bdb9
Salman Ghouse <ghousemu@sheridancollege.ca>
2022-03-25 12:04:31 -0400

    finished frntend signup login

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -1,30 +1,46 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
-
 const Login = () => {
   return (
-    <div className="">
-      <form>
-        <label>
-          Username
-          <input type="text" />
-        </label>
+    <div class="flex">
+      <div class="items-center flex-none content-center" Style="margin:0 auto;">
+        <div class="card w-96 glass flex-shrink ml-6 mr-6 mt-6">
+          <figure>
+            <div class="form-control w-full max-w-xs">
+              <label class="label">
+                <span class="label-text">Email/Username</span>
+              </label>
+              <input
+                type="email"
+                placeholder="Type here"
+                class="input input-bordered w-full max-w-xs"
+              ></input>
 
-        <label>
-          Username
-          <input type="email" />
-        </label>
-
-        <label>
-          Username
-          <input type="password" />
-        </label>
-      </form>
-      <Link to="/user/signup">
-        <p>Signup</p>
-      </Link>
+              <label class="label">
+                <span class="label-text-alt">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="Type here"
+                class="input input-bordered w-full max-w-xs"
+              ></input>
+            </div>
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title text-white">Login</h2>
+            <p class="text-secondary">Enter Login Details</p>
+            <div class="card-actions justify-end">
+              <button class="btn btn-primary">Login</button>
+            </div>
+            <div class="card-actions justify-start">
+              <Link to="/user/signup">
+                <div class="badge badge-outline-primary">Signup</div>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- client/src/components/signup/signup.js
+++ client/src/components/signup/signup.js
@@ -2,40 +2,64 @@
 // import { ADD_USER } from "../../utils/mutations";
 // import Auth from "../../utils/auth";
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Button } from "@mantine/core";
 
 const Signup = () => {
   return (
     <main>
       <div>
-        <div>
-          <h4>Sign Up</h4>
-          <div>
-            <form onSubmit="">
-              <input />
+        <h4>Sign Up</h4>
+        <div
+          class="card w-96 glass flex-shrink ml-6 mr-6 mt-6"
+          Style="margin:0 auto;"
+        >
+          <figure>
+            <div class="form-control w-full max-w-xs">
+              <label class="label">
+                <span class="label-text">Username</span>
+              </label>
               <input
-                placeholder="Your email"
-                name="email"
+                type="text"
+                placeholder="Type here"
+                class="input input-bordered w-full max-w-xs"
+              ></input>
+              <label class="label">
+                <span class="label-text">Email</span>
+              </label>
+
+              <input
                 type="email"
-                id="email"
-                value="{formState.email}"
-              />
+                placeholder="Type here"
+                class="input input-bordered w-full max-w-xs"
+              ></input>
+
+              <label class="label">
+                <span class="label-text-alt">Password</span>
+              </label>
               <input
-                placeholder="******"
-                name="password"
                 type="password"
-                id="password"
-                value="{formState.password}"
-              />
-              <Button color="cyan" type="submit">Submit</Button>
-            </form>
+                placeholder="Type here"
+                class="input input-bordered w-full max-w-xs"
+              ></input>
+            </div>
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title text-white">Signup</h2>
+            <p class="text-secondary">Fill Out</p>
+            <div class="card-actions justify-end">
+              <Link to="">
+                <button class="btn btn-primary">Signup</button>
+              </Link>
+            </div>
+            <div class="card-actions justify-start">
+              <Link to="/user/login">
+                <div class="badge badge-outline-primary">Login</div>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
-      <Link to="/user/login">
-        <p>Login</p>
-      </Link>
     </main>
   );
 };
