82566f09c064a204dd5319383120e4b78fdb9753
Salman Ghouse <ghousemu@sheridancollege.ca>
2022-03-26 14:36:57 -0400

    models not getting created db

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,5 @@
+const resolvers = {
+  Query: {
+    // logic here remains the same
+  },
+};
--- server/server.js
+++ server/server.js
@@ -1,43 +1,16 @@
 const express = require("express");
-// import ApolloServer
-const { ApolloServer } = require("apollo-server-express");
-const routes = require("./routes");
-// import our typeDefs and resolvers
-// const { typeDefs, resolvers } = require("./schemas");
-// const db = require("./config/connection");
-// const typeDefs = "";
-// const resolvers = "";
-
-const cardData = require("./routes/api/carddata");
-const PORT = process.env.PORT || 3001;
+const mongoose = require("mongoose");
+const db = require("./config/connection");
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-// const startServer = async () => {
-//   // create a new Apollo server and pass in our schema data
-//   const server = new ApolloServer({
-//     typeDefs,
-//     resolvers,
-//     // context: authMiddleware,
-//   });
-
-//   // Start the Apollo server
-//   await server.start();
-
-//   // integrate our Apollo server with the Express application as middleware
-//   server.applyMiddleware({ app });
-
-//   // log where we can go to test our GQL API
-//   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-// };
-
-// // Initialize the Apollo server
-// startServer();
-
-app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-// app.use(routes);
+app.use(require("./Routes"));
 
-app.listen(PORT, () => {
-  console.log(`API server running on port ${PORT}!`);
+// Use this to log mongo queries being executed!
+db.once("open", () => {
+  app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
 });
