5f08dd6873444751a7dc8203346e74f3b73da5db
Salman Ghouse <57410051+salmanghouse1@users.noreply.github.com>
2022-03-29 19:30:07 -0400

    Revert "Feature/product seeds"

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,12 +1,9 @@
 import React from "react";
 import { Routes, Route, Link, Outlet } from "react-router-dom";
-import Cart from "../Cart/";
-import Auth from "../../utils/auth";
 
-function Header(){
-  
-  if(Auth.loggedIn()){
+const Header = () => {
   return (
+    // Nav routes
     <div class="container">
       <div>
         <header className="header">
@@ -30,65 +27,10 @@ function Header(){
                 </Link>
               </li>
               <li>
-                <Link to="/login" class="btn btn-ghost normal-case text-md">
-                  Login to see Card Prices
-                </Link>
-              </li>
-              <li>
-                <Link to="/user" class="btn btn-ghost normal-case text-md">
-                  User
-                </Link>
-              </li>
-              <li>
-                <Link to="/contact" class="btn btn-ghost normal-case text-md">
-                  Contact
-                </Link>
-              </li>
-              
-              <li>
-                  <Cart/>
-                
-              </li>
-              
-              
-            </ul>
-          </nav>
-        </header>
-      </div>
-      <div>
-        
-        <Outlet />
-      </div>
-    </div>
-
-    )
-    
-}else{
-  return(
-    <div class="container">
-      <div>
-        <header className="header">
-          <nav class="navbar bg-base-100" Style="background:#131A22!important">
-            <Link class="normal-case text-white primary-content" to="/">
-              <h1>Pokemon Encyclopedia and Card Market</h1>
-            </Link>
-
-            <ul Style="background:purple;" class="m-6">
-              <li>
-                <Link to="/" class="btn btn-ghost normal-case text-md">
-                  Home
-                </Link>
-              </li>
-              <li>
                 <Link
-                  to="/game-stats"
+                  to="/user/login"
                   class="btn btn-ghost normal-case text-md"
                 >
-                  GameStats
-                </Link>
-              </li>
-              <li>
-                <Link to="/login" class="btn btn-ghost normal-case text-md">
                   Login to see Card Prices
                 </Link>
               </li>
@@ -102,18 +44,15 @@ function Header(){
                   Contact
                 </Link>
               </li>
-              
-              
             </ul>
           </nav>
         </header>
       </div>
       <div>
-        
         <Outlet />
       </div>
     </div>
   );
-}};
+};
 
 export default Header;
