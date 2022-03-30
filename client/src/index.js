1b052f68fb5b0fa056e40aedfae86f71a78a3f7c
Salman Ghouse <ghousemu@sheridancollege.ca>
2022-03-30 11:37:02 -0400

    deploying test

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
-import * as serviceWorker from "./serviceWorker";
+// import { BrowserRouter } from "react-router-dom";
+// import * as serviceWorker from "./serviceWorker";s
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <App />,
+
   document.getElementById("root")
 );
 
@@ -16,4 +15,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-serviceWorker.register();
+// serviceWorker.register();
