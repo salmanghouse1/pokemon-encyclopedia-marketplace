cd0b7d658776bc8b3e8fc765a9aed504f517629f
Salman Ghouse <ghousemu@sheridancollege.ca>
2022-05-16 23:53:46 -0400

    onclick doesnt rerender anymore

--- client/src/components/LikeButton/index.js
+++ client/src/components/LikeButton/index.js
@@ -1,15 +1,20 @@
 import { ADD_TO_WISHLIST } from "../../utils/mutations";
 
-function LikeButton({ user, id, likeCount, likes }) {
+function LikeButton({ postId, id, Image, Name }) {
   const [liked, setLiked] = useState(false);
 
   const [likePost] = useMutation(ADD_TO_WISHLIST, {
-    variables: { postId: id },
+    variables: { postId: postId, id: id, Image: Image, Name: Name },
   });
 
   return (
     <div>
-      <div className="badge badge-outline-primary" onClick={likePost}>
+      <div
+        className="badge badge-outline-primary"
+        onClick={() => {
+          likePost;
+        }}
+      >
         ❤️Wishlist
       </div>
     </div>
